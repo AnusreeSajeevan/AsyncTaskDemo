9db41ba35a9a4af721ebd952229b617237575f30
Anusree <anusree@wizroots.com>
2017-12-01 16:30:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/anu/asynctaskdemo/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.anu.asynctaskdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnStart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnStart = findViewById(R.id.btn_start);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
